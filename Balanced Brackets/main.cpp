f59eb259d552a5c7d0e80b5e897c41d4d7c94c99
Josh Collins <jjcollins2@crimson.ua,edu>
2017-10-18 17:16:16 -0500

    First creation of bracket balance program.

--- Balanced Brackets/main.cpp
+++ Balanced Brackets/main.cpp
@@ -6,10 +6,58 @@
 //  Copyright © 2017 Josh Collins. All rights reserved.
 //
 
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello World!\n";
+using namespace std;
+
+bool is_balanced(string expression) {
+    stack <char> s;
+    for(int x = 0; x < expression.size(); x++)  {
+        if(s.top() == NULL)
+            s.push(expression[x]);
+        else if(s.top() == expression[x])
+            s.pop();
+        else
+            s.push(expression[x]);
+    }
+    
+    if(s.size() == 0)
+        return true;
+    else
+        return false;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        string expression;
+        cin >> expression;
+        bool answer = is_balanced(expression);
+        if(answer)
+            cout << "YES\n";
+        else cout << "NO\n";
+    }
     return 0;
 }
